2d2ba21f474042fd7a9c6985ab7b8ade96f4af37
Felipe Garcia Suez <garciasuezfelipe@gmail.com>
2023-11-13 00:37:03 -0300

    avance generales

--- js/common.js
+++ js/common.js
@@ -2,17 +2,34 @@ const tipoRecuento = 1;
 var tipoEleccion = 0;
 url = window.location.href;
 
+//Identificamos si es una eleccion de tipo PASO o Generales
 if(url.substring(url.lastIndexOf("/") + 1) == "paso.html"){
     tipoEleccion = 1;
 }else if(url.substring(url.lastIndexOf("/") + 1) == "generales.html"){
     tipoEleccion = 2;
 }
 
-
+//Seleccionamos los elementos del DOM
 const añoSelect = document.getElementById("añoSelect");
+const cargoSelect = document.getElementById("cargoSelect");
 
 
+//Funciones
 
+const agregarOpciones =async (elemento, func) =>{
+    const datos = await func;
+    console.log(datos);
+    datos.sort()
+    datos.forEach((dato) => {
+        const option = document.createElement("option");
+        option.value = dato;
+        option.text = dato;
+        elemento.add(option);
+    });
+
+}
+
+//Funcion para buscar los periodos
 const buscarPeriodos = async () => {
     try {
         const res = await fetch(
@@ -27,20 +44,9 @@ const buscarPeriodos = async () => {
     
     }
 
+//CARGAMOS LOS AÑOS AUTOMATICAMENTE    
+agregarOpciones(añoSelect, buscarPeriodos())
 
-const agregarOpcionesAñoSelect = async () => {
-    const periodos = await buscarPeriodos();
-    console.log(periodos);
-    periodos.sort();
-    periodos.forEach((periodo) => {
-        const option = document.createElement("option");
-        option.value = periodo;
-        option.text = periodo;
-        añoSelect.add(option);
-    });
-};
-
-agregarOpcionesAñoSelect();
 
 const buscarDatos = async (periodoSelect) =>{
     if(periodoSelect && periodoSelect != "Año"){
@@ -57,32 +63,58 @@ const buscarDatos = async (periodoSelect) =>{
     }
 }
 
-const filtrarCargos = async (datos) => {
-    datos.filter((eleccion) => {
-        if(eleccion.IdEleccion == tipoEleccion){
-            console.log(eleccion)
-            cargosFiltrados= eleccion.Cargos.forEach((cargo) => {
-                console.log(cargo)
-                return {
-                    IdCargo: cargo.IdCargo,
-                    Cargo: cargo.Cargo
-                };
-            })
-        }
-    });
-    return cargosFiltrados;
-}
+//FUNCION QUE SE EJECUTA CUANDO SE CAMBIA EL AÑO
 
-
-const obtenerCargos = async () => {
+añoSelect.addEventListener("change",async () => {
     const datos = await buscarDatos(añoSelect.value);
     
-    // Guardar los datos en una variable
-    cargosFiltrados = await filtrarCargos(datos);
-    console.log(cargosFiltrados);
-    };
-    
-añoSelect.addEventListener("change", () => {
-    obtenerCargos();
-    }
+    // Guardar los datos filtrados en una variable
+    cargos = await filtrarCargos(datos);
+    if(cargos)
+        cambiarCargos(cargos);
+    }    
 )
+
+//COMBO CARGOS ---------------------------------------------
+var dataCargos
+const filtrarCargos = async (datos) => {
+    cargosFiltrados = []
+    if(datos){
+        datos.forEach((eleccion) => {
+            if(eleccion.IdEleccion == tipoEleccion){
+                eleccion.Cargos.forEach((cargo) => {
+                    cargosFiltrados.push(cargo)
+                })
+            }
+        });
+        dataCargos = cargosFiltrados.sort()
+        return dataCargos;
+    }
+}
+
+const cambiarCargos = async (cargos) => {
+    cargos.forEach((cargo) => {
+        const option = document.createElement("option");
+        option.value = cargo.IdCargo;
+        option.text = cargo.Cargo;
+        cargoSelect.add(option);
+    });
+}
+
+//FUNCION QUE SE EJECUTA CUANDO SE CAMBIA EL CARGO
+
+cargoSelect.addEventListener("change", async () => {
+    const cargoId = cargoSelect.value;
+    console.log(dataCargos)
+    
+    const cargoSeleccionado = dataCargos.find((cargo) => {
+        if(cargo.IdCargo == cargoId){
+            return cargo
+        }
+    })
+    console.log(cargoSeleccionado)
+    
+})
+
+
+// COMBO DISTRITO --------------------------------------------
